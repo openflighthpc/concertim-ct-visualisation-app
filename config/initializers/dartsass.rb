1876372ba8df37f876cd5f0274a9eb99cb543ea3
Ben Armston <ben.armston@alces-software.com>
2023-01-25 13:26:05 +0000

    Fix foundation styles

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,2 @@
-# Rails.application.config.dartsass.builds = {
-#   "app/index.sass"  => "app.css",
-#   "site.scss"       => "site.css"
-# }
-
-# Rails.application.config.dartsass.build_options << " --load-path #{Rails.root.join("vendor/foundation/stylesheets")}"
+Rails.application.config.dartsass.build_options << " --load-path #{Rails.root.join("vendor/foundation/stylesheets")}"
+# Rails.application.config.dartsass.build_options << " --load-path #{Rails.root.join("app/assets/stylesheets/vendor/")}"
