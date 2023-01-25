e6bc8d352f24c05a6e97d951f0b1ef3fa8a4355b
Ben Armston <ben.armston@alces-software.com>
2023-01-25 12:19:55 +0000

    WIP: Attempt to add foundation styles

---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,6 @@
+# Rails.application.config.dartsass.builds = {
+#   "app/index.sass"  => "app.css",
+#   "site.scss"       => "site.css"
+# }
+
+# Rails.application.config.dartsass.build_options << " --load-path #{Rails.root.join("vendor/foundation/stylesheets")}"
