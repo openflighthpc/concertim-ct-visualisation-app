861fa01b699cb83a20aa0d170e7430b393d10c7a
Ben Armston <ben.armston@alces-software.com>
2023-03-02 14:22:22 +0000

    Remove hardcoded placeholder can? method

--- uma/app/models/uma/user.rb
+++ uma/app/models/uma/user.rb
@@ -25,9 +25,5 @@ module Uma
     devise :database_authenticatable, :registerable,
       :recoverable, :rememberable, :validatable,
       :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
-
-    def can?(action, resource)
-      true
-    end
   end
 end
