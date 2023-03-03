034fa10577529e28fe32086f761ba5251ae5f965
Ben Armston <ben.armston@alces-software.com>
2023-03-03 10:42:49 +0000

    Fix hacks controller

--- core/app/controllers/api/v1/hacks_controller.rb
+++ core/app/controllers/api/v1/hacks_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::HacksController < Api::V1::ApplicationController
   def restart_delayed_job
     authorize! :restart, :delayed_job
-    `/usr/bin/systemctl restart delayed_job`
-    render json: {status: 'OK'}, status: :ok
+    Rails.logger.info("Restarting delayed job")
+    Rails.logger.info(`sudo /usr/bin/systemctl restart delayed_job 2>&1`)
+    if $?.exitstatus == 0
+      render json: {status: 'OK'}, status: :ok
+    else
+      render json: {status: 'FAILED'}, status: :internal_server_error
+    end
   end
 end
