179c732e9b900605d7ba32ebd971559b56022a78
Ben Armston <ben.armston@alces-software.com>
2023-03-10 16:32:50 +0000

    Fix issue with rack creation example script

--- docs/api/examples/create-rack.sh
+++ docs/api/examples/create-rack.sh
@@ -20,42 +20,46 @@ AUTH_TOKEN=${AUTH_TOKEN:-$("${SCRIPT_DIR}"/get-auth-token.sh)}
 # If it is the first, we need to specify the name and height of the rack.  If
 # it is not the first, we don't want to specify them, and instead use the
 # defaults that are calculated on the details of the last created rack.
-NUM_RACKS=$(
-curl -s -k \
-    -H 'Content-Type: application/json' \
-    -H "Authorization: Bearer ${AUTH_TOKEN}" \
-    -X GET "${BASE_URL}/racks" \
-    | jq -r length
-)
+NUM_RACKS=$("${SCRIPT_DIR}/list-racks.sh" | jq -r length)
 
 if [ "${NUM_RACKS}" == "0" ] ; then
-    # If we don't yet have any racks we create a body with the name,
-    # description and U height.  The name and description are both required.
+    # If we don't yet have any racks we create a body with the name and U
+    # height.
     BODY=$(jq --null-input \
         --arg name "Rack-1" \
-        --arg description "A rack" \
         --arg u_height 42 \
-        '{"name": $name, "description": $description, "u_height": $u_height|tonumber}'
-    )
-    EXTRA_ARGS="-H 'Content-Type: application/json' -d \"${BODY}\""
-else
-    # If we already have some racks defined, we send an empty body to use
-    # defaults based on those provided for the last created rack.
-    # BODY=""
-    # EXTRA_ARGS="-d \"${BODY}\""
-    EXTRA_ARGS="-d ''"
+        '
+{
+  "name": $name,
+  "u_height": $u_height|tonumber
+}
+'
+)
 fi
 
 # Run curl with funky redirection to capture response body and status code.
 BODY_FILE=$(mktemp)
 HTTP_STATUS=$(
-curl -s -k \
-    -w "%{http_code}" \
-    -o >(cat > "${BODY_FILE}") \
-    -H "Accept: application/json" \
-    -H "Authorization: Bearer ${AUTH_TOKEN}" \
-    -X POST "${BASE_URL}/racks" \
-    ${EXTRA_ARGS}
+if [ "${NUM_RACKS}" == "0" ] ; then
+    curl -s -k \
+        -w "%{http_code}" \
+        -o >(cat > "${BODY_FILE}") \
+        -H 'Content-Type: application/json' \
+        -H "Accept: application/json" \
+        -H "Authorization: Bearer ${AUTH_TOKEN}" \
+        -X POST "${BASE_URL}/racks" \
+        -d "${BODY}"
+else
+    # If we already have some racks defined, we send an empty body to use
+    # defaults based on those provided for the last created rack.
+    curl -s -k \
+        -w "%{http_code}" \
+        -o >(cat > "${BODY_FILE}") \
+        -H "Accept: application/json" \
+        -H "Authorization: Bearer ${AUTH_TOKEN}" \
+        -X POST "${BASE_URL}/racks" \
+        -d ''
+fi
 )
 
 if [ "${HTTP_STATUS}" == "200" ] || [ "${HTTP_STATUS}" == "201" ] ; then
