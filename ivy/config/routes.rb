34ba8e3708dab9daf00ba57148477b67600890a5
Ben Armston <ben.armston@alces-software.com>
2023-03-26 15:39:07 +0100

    Accounts detail page; user can change password

--- ivy/config/routes.rb
+++ ivy/config/routes.rb
@@ -1,5 +1,5 @@
 Ivy::Engine.routes.draw do
-  resource :irv, as: :'ivy_irv', only: :show do
+  resource :irv, only: :show do
     get :configuration
   end
 end
--- uma/config/routes.rb
+++ uma/config/routes.rb
@@ -1,3 +1,7 @@
 Uma::Engine.routes.draw do
-  devise_for :users, class_name: "Uma::User"
+  devise_for :users, class_name: "Uma::User", only: [:sessions]
+  as :user do
+    get 'users/edit' => 'registrations#edit', :as => 'edit_user_registration'
+    match 'users' => 'registrations#update', :as => 'user_registration', via: [:put, :patch]
+  end
 end
