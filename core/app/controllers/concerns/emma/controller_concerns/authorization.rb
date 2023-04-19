55c4d7c9795f42741b10c9b0c4512457b1d6eedd
Ben Armston <ben.armston@alces-software.com>
2023-04-19 14:14:44 +0100

    Better unauthorized error message

--- core/app/controllers/concerns/emma/controller_concerns/authorization.rb
+++ core/app/controllers/concerns/emma/controller_concerns/authorization.rb
@@ -12,7 +12,7 @@ module Emma
         rescue_from CanCan::AccessDenied do |exception|
           Rails.logger.info "User #{current_user.id} does not have permission to #{exception.action} #{exception.subject}"
           @offending_action = exception.action
-          @offending_subject = exception.subject
+          @offending_subject = exception.subject.name rescue exception.subject
 
           respond_to do |format|
             format.html { render "errors/403", status: 403 }
