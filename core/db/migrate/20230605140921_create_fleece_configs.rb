ec80540e7a64c2497fd96f370efeb0c75bbaacc2
Ben Armston <ben.armston@alces-software.com>
2023-06-07 17:30:33 +0100

    Fix issue with fleece config migration
    Need to reset `search_path` to avoid it being created in the `ivy`
    schema.

--- core/db/migrate/20230605140921_create_fleece_configs.rb
+++ core/db/migrate/20230605140921_create_fleece_configs.rb
@@ -1,5 +1,9 @@
 class CreateFleeceConfigs < ActiveRecord::Migration[7.0]
   def change
+    reversible do |dir|
+      dir.up   { execute 'SET search_path TO public' }
+    end
+
     create_table :fleece_configs do |t|
       t.string :host_name,    limit: 255, null: false
       t.inet :host_ip,                    null: false
@@ -10,6 +14,10 @@ class CreateFleeceConfigs < ActiveRecord::Migration[7.0]
       t.string :domain_name,  limit: 255, null: false
 
       t.timestamps
+
+      reversible do |dir|
+        dir.down { execute 'SET search_path TO ivy,public' }
+      end
     end
   end
 end
