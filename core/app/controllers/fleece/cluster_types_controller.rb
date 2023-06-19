afaf6b93d4b6534449e45de6227572475e7395c7
timalces <tim.brackfield@alces-software.com>
2023-06-19 12:04:30 +0100

    don't show cluster type selection if no config set

--- core/app/controllers/fleece/cluster_types_controller.rb
+++ core/app/controllers/fleece/cluster_types_controller.rb
@@ -2,5 +2,6 @@ class Fleece::ClusterTypesController < ApplicationController
   load_and_authorize_resource :cluster_type, class: Fleece::ClusterType
 
   def index
+    @config = Fleece::Config.first
   end
 end
