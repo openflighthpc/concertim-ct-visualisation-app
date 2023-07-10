64322a27c2537f06ee5d0d4de11a66696c1a3985
Ben Armston <ben.armston@alces-software.com>
2023-07-10 18:39:55 +0100

    Update create-and-populate-rack example script

--- docs/api/examples/create-and-populate-rack.sh
+++ docs/api/examples/create-and-populate-rack.sh
@@ -25,14 +25,9 @@ echo "Created empty rack ${RACK_NAME}" >&2
 "${SCRIPT_DIR}/show-rack.sh" "${RACK_ID}"
 echo
 
-# Prefix nodes in the first rack with `1`, nodes in the second rack with `2`
-# etc..
-NUM_RACKS=$("${SCRIPT_DIR}/list-racks.sh" | jq 'length')
-NAME_PREFIX=$( echo "${RACK_NAME}" | tr -dc 0-9 )
-
 # Create a badly named and located device in that empty rack.
 LARGE_TEMPLATE_ID=4
-OUTPUT=$("${SCRIPT_DIR}/create-device.sh" comp-${NAME_PREFIX}01 "${RACK_ID}" f 11 "${LARGE_TEMPLATE_ID}")
+OUTPUT=$("${SCRIPT_DIR}/create-device.sh" comp-101 "${RACK_ID}" f 11 "${LARGE_TEMPLATE_ID}")
 if [ $? -ne 0 ] ; then
     # Errors will have been sent to stderr.
     exit
@@ -43,7 +38,7 @@ echo "Added badly named and located device to rack" >&2
 echo
 
 # Correct the name of the device.
-OUTPUT=$("${SCRIPT_DIR}/update-device.sh" "${DEVICE_ID}" comp${NAME_PREFIX}01)
+OUTPUT=$("${SCRIPT_DIR}/update-device.sh" "${DEVICE_ID}" comp101)
 if [ $? -ne 0 ] ; then
     # Errors will have been sent to stderr.
     exit
@@ -62,7 +57,7 @@ echo "Moved device" >&2
 "${SCRIPT_DIR}/show-device.sh" "${DEVICE_ID}"
 echo
 
-./populate-rack.sh ${RACK_ID} 4 39 comp${NAME_PREFIX}02
+./populate-rack.sh ${RACK_ID} 4 39 comp102
 
 "${SCRIPT_DIR}/show-rack.sh" "${RACK_ID}"
 echo
