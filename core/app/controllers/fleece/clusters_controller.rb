9cf91ed57fa6b73fa9b98330dc383202d339e89f
timalces <tim.brackfield@alces-software.com>
2023-07-11 15:00:06 +0100

    logic fixes

--- core/app/controllers/fleece/clusters_controller.rb
+++ core/app/controllers/fleece/clusters_controller.rb
@@ -1,24 +1,30 @@
 class Fleece::ClustersController < ApplicationController
   def new
     authorize! :create, Fleece::Cluster
-    @cluster_type = Fleece::ClusterType.find_by_foreign_id(params[:cluster_type_foreign_id])
+    @cluster_type = Fleece::ClusterType.find_by_foreign_id!(params[:cluster_type_foreign_id])
     @cluster = Fleece::Cluster.new(cluster_type: @cluster_type)
   end
 
   def create
     authorize! :create, Fleece::Cluster
     @config = Fleece::Config.first
+    @cluster_type = Fleece::ClusterType.find_by_foreign_id!(params[:cluster_type_foreign_id])
+    @cluster = Fleece::Cluster.new(
+      cluster_type: @cluster_type, name: permitted_params[:name], cluster_params: permitted_params[:cluster_params]
+    )
 
     if @config.nil?
-      flash[:alert] = "Unable to send cluster configuration: cloud environment config not set"
+      flash.now.alert = "Unable to send cluster configuration: cloud environment config not set"
       render action: :new
       return
     end
 
-    @cluster_type = Fleece::ClusterType.find_by_foreign_id(params[:cluster_type_foreign_id])
-    @cluster = Fleece::Cluster.new(
-      cluster_type: @cluster_type, name: permitted_params[:name], cluster_params: permitted_params[:cluster_params]
-    )
+    unless current_user.project_id
+      flash.now.alert = "Unable to send cluster configuration: you do not yet have a project id"
+      render action: :new
+      return
+    end
+
     if !@cluster.valid?
       render action: :new
       return
@@ -27,10 +33,10 @@ class Fleece::ClustersController < ApplicationController
     result = Fleece::CreateClusterJob.perform_now(@cluster, @config, current_user)
 
     if result.success?
-      flash[:success] = "Cluster configuration sent"
+      flash.now.success = "Cluster configuration sent"
       redirect_to ivy_engine.irv_path
     else
-      flash[:alert] = "Unable to send cluster configuration: #{result.error_message}"
+      flash.now.alert = "Unable to send cluster configuration: #{result.error_message}"
       render action: :new
     end
   end
