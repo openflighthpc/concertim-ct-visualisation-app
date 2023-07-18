0b3dae1c3b11094b8d8b9eac08948f56c911e550
timalces <tim.brackfield@alces-software.com>
2023-07-18 16:46:26 +0100

    improved/fixed flash messages

--- core/app/controllers/fleece/clusters_controller.rb
+++ core/app/controllers/fleece/clusters_controller.rb
@@ -12,7 +12,7 @@ class Fleece::ClustersController < ApplicationController
     use_cache = params[:use_cache] != "false"
     result = Fleece::SyncIndividualClusterTypeJob.perform_now(@config, @cluster_type, use_cache)
     unless result.success?
-      flash[:alert] = result.error_message
+      flash[:alert] = "#{result.error_message} - please contact an admin"
       redirect_to fleece_cluster_types_path(use_cache: false)
       return
     end
@@ -28,13 +28,14 @@ class Fleece::ClustersController < ApplicationController
     )
 
     if @config.nil?
-      flash.now.alert = "Unable to send cluster configuration: cloud environment config not set"
+      flash.now.alert = "Unable to send cluster configuration: cloud environment config not set. Please contact an admin"
       render action: :new
       return
     end
 
     unless current_user.project_id
-      flash.now.alert = "Unable to send cluster configuration: you do not yet have a project id"
+      flash.now.alert = "Unable to send cluster configuration: you do not yet have a project id. " \
+                        "This will be added automatically shortly."
       render action: :new
       return
     end
@@ -47,7 +48,7 @@ class Fleece::ClustersController < ApplicationController
     result = Fleece::CreateClusterJob.perform_now(@cluster, @config, current_user)
 
     if result.success?
-      flash.now.success = "Cluster configuration sent"
+      flash[:success] = "Cluster configuration sent"
       redirect_to ivy_engine.irv_path
     else
       flash.now.alert = "Unable to send cluster configuration: #{result.error_message}"
