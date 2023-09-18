1c2f12f3500f5407440a6dee2309363cfdc6abf8
timalces <tim.brackfield@alces-software.com>
2023-09-18 17:39:11 +0100

    updated navigation logic

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,4 +1,5 @@
 SimpleNavigation::Configuration.run do |navigation|
+  url_helpers = Rails.application.routes.url_helpers
   navigation.selected_class = 'current'
 
   navigation.items do |primary|
@@ -15,29 +16,29 @@ SimpleNavigation::Configuration.run do |navigation|
         align: :right,
         icon: :youraccount,
         highlights_on: /\/users/ do |acc|
-          acc.item :acc_details, 'Account details', Rails.application.routes.url_helpers.edit_user_registration_path, :icon => :details, :link => {:class => 'details'}
+          acc.item :acc_details, 'Account details', url_helpers.edit_user_registration_path, :icon => :details, :link => {:class => 'details'}
           unless current_user.root?
-            acc.item :acc_details, 'Manage key-pairs', Rails.application.routes.url_helpers.key_pairs_path,
+            acc.item :acc_details, 'Manage key-pairs', url_helpers.key_pairs_path,
                      :icon => :key, :link => {:class => 'details'}
           end
-          acc.item :acc_logout, 'Log out', Rails.application.routes.url_helpers.destroy_user_session_path, :icon => :logout, :link => {:class => 'logout'}
+          acc.item :acc_logout, 'Log out', url_helpers.destroy_user_session_path, :icon => :logout, :link => {:class => 'logout'}
         end
 
-      primary.item :infra_racks_list, 'Rack view', Rails.application.routes.url_helpers.interactive_rack_views_path, icon: :infra_racks, :link => {class: 'infra_racks'}
+      primary.item :infra_racks_list, 'Rack view', url_helpers.interactive_rack_views_path, icon: :infra_racks, :link => {class: 'infra_racks'}
 
       if current_user.root?
-        primary.item :config, 'Cloud environment', Rails.application.routes.url_helpers.cloud_service_config_path,
+        primary.item :config, 'Cloud environment', url_helpers.cloud_service_config_path,
           icon: :config,
           highlights_on: /\/cloud-env\/configs/
       end
 
       if current_user.can?(:create, Cluster)
-        primary.item :cluster_types, 'Launch cluster', Rails.application.routes.url_helpers.cluster_types_path,
+        primary.item :cluster_types, 'Launch cluster', url_helpers.cluster_types_path,
           icon: :racks,
           highlights_on: /\/cloud-env\/(cluster-types|clusters)/
       end
     else
-      primary.item :login, 'Log in', Rails.application.routes.url_helpers.new_user_session_path,
+      primary.item :login, 'Log in', url_helpers.new_user_session_path,
         icon: :login,
         align: :right,
         highlights_on: /\/users\/sign_in/
