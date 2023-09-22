1547f0464f837b131fd7e81dcb65f8f37cd09b22
timalces <tim.brackfield@alces-software.com>
2023-09-22 16:43:21 +0100

    fixed drag policy test

--- spec/javascript/drag_policy.spec.js
+++ spec/javascript/drag_policy.spec.js
@@ -3,45 +3,29 @@ import AssetManager from 'canvas/irv/util/AssetManager'
 import DragPolicy from 'canvas/irv/util/DragPolicy'
 import Rack from 'canvas/irv/view/Rack'
 import RackObject from 'canvas/irv/view/RackObject'
-import Machine from 'canvas/irv/view/Machine'
 import Profiler from 'Profiler'
-
 import 'mia/contrib/knockout.js'
 
-let rackData = {
-    "owner": {
-        "id": "5",
-        "name": "turnip",
-        "login": "turnip"
-    },
-    "template": {
-        "id": "1",
-        "name": "rack",
-        "rackable": "3",
-        "images": {
-            "rack_rear_top": "misc_rack_600_rear_top.png",
-            "rack_repeat_1": "misc_rack_600_repeat_1.png",
-            "rack_front_top": "misc_rack_600_front_top.png",
-            "rack_rear_bottom": "misc_rack_600_rear_bottom.png",
-            "rack_front_bottom": "misc_rack_600_front_bottom.png"
-        },
-        "height": "42",
-        "depth": "2",
-        "padding_left": "0",
-        "padding_right": "0",
-        "padding_top": "0",
-        "padding_bottom": "0",
-        "simple": "true"
-    },
-    "id": "1",
-    "name": "Rack-1",
-    "uHeight": "42",
+// I've taken this from some live data, some may be unnecessary for this testing
+const deviceData = {
+    "id": 1,
+    "name": "comp102",
     "buildStatus": "IN_PROGRESS",
     "cost": "$0.00",
-    "nextRackId": "2"
+    "instances": [],
+    "column": 0,
+    "row": 0,
+    "facing": "f",
+    "template": {
+        "images": {},
+        "width": 1,
+        "height": 1,
+        "rotateClockwise": true
+    },
+    "focused": false
 }
 
-let chassisData = {
+const chassisData = {
         "template": {
             "id": 1,
             "name": "Large",
@@ -57,7 +41,7 @@ let chassisData = {
             "simple": true,
             "padding": {"left": 0, "right": 0, "top": 0, "bottom": 0}
         },
-        "Slots": [rackData],
+        "Slots": [{"Machine": deviceData}],
         "id":"1",
         "name":"Large-Rack-2-16953117339250503",
         "type":"RackChassis",
@@ -70,11 +54,44 @@ let chassisData = {
         "instances":[]
 };
 
-// circular logic?
-rackData['chassis'] = [chassisData];
+const rackData = {
+    "owner": {
+        "id": 5,
+        "name": "turnip",
+        "login": "turnip"
+    },
+    "template": {
+        "id": 1,
+        "name": "rack",
+        "rackable": 3,
+        "images": {
+            "rack_rear_top": "misc_rack_600_rear_top.png",
+            "rack_repeat_1": "misc_rack_600_repeat_1.png",
+            "rack_front_top": "misc_rack_600_front_top.png",
+            "rack_rear_bottom": "misc_rack_600_rear_bottom.png",
+            "rack_front_bottom": "misc_rack_600_front_bottom.png"
+        },
+        "height": 42,
+        "depth": 2,
+        "padding_left": 0,
+        "padding_right": 0,
+        "padding_top": 0,
+        "padding_bottom": 0,
+        "simple": "true"
+    },
+    "id": 1,
+    "name": "Rack-1",
+    "uHeight": 42,
+    "buildStatus": "IN_PROGRESS",
+    "cost": "$0.00",
+    "nextRackId": 2,
+    "chassis": [chassisData]
+}
 
-test('Make a rack', () => {
-    let fakeImage = { height: 99 }
+// lots of class variables that require values before instantiating objects works
+beforeAll(() => {
+    // Pretend images, with minimal required data
+    const fakeImage = { height: 99 }
     Rack.IMAGES_BY_TEMPLATE[1] = {};
     Rack.IMAGES_BY_TEMPLATE[1].slices = {
         front: {
@@ -88,22 +105,34 @@ test('Make a rack', () => {
             repeat : fakeImage,
         }
     };
+
     AssetManager.CACHE[RackObject.IMAGE_PATH + chassisData.template.images.front] = fakeImage;
     AssetManager.CACHE[RackObject.IMAGE_PATH + chassisData.template.images.rear] = fakeImage;
 
+    // Pretend front end element
     RackObject.RACK_GFX = {"containerEl": {}};
 
-   Profiler.makeCompatible();
-   Profiler.LOG_LEVEL  = Profiler.INFO;
-   Profiler.TRACE_ONLY = true;
-   RackObject.MODEL = new ViewModel();
-   RackObject.MODEL.deviceLookup({"racks": {}, "devices": {}, "chassis": {}})
+    RackObject.MODEL = new ViewModel();
+    RackObject.MODEL.deviceLookup({"racks": {}, "devices": {}, "chassis": {}})
 
-   // dont try to draw image
-   const mockedFunction = jest.fn();
-   jest.spyOn(Rack.prototype, 'setImageCache').mockImplementation(mockedFunction);
+    // Profiler uses deprecated functions/code that under normal circumstances would break.
+    // Perhaps it would be useful to have the option not to use it.
+    Profiler.makeCompatible();
+    // Try to limit what profiler logs
+    Profiler.LOG_LEVEL  = Profiler.CRITICAL;
+    Profiler.TRACE_ONLY = true;
 
-   let rack = new Rack(rackData, "racks", 1);
+    // Dont try to draw images, as no canvas to draw. We could mock one, but not
+    // relevant to this test
+    const mockedFunction = jest.fn();
+    jest.spyOn(Rack.prototype, 'setImageCache').mockImplementation(mockedFunction);
+});
+
+test("filter function returns array containing just device's rack", () => {
+   // There may be an easier/ better way to generate a machine directly, but there are so, so many dependencies
+   const rack = new Rack(rackData, "racks", 1);
+   const machine = rack.children[0].children[0];
+   expect(DragPolicy.filter(machine)).toEqual([rack]);
 });
 
 
