723e8c79131b3732acf7cba88ca75d32d3d2fc4a
Ben Armston <ben.armston@alces-flight.com>
2023-09-27 15:48:10 +0100

    Merge DragPolicy spec files into one

--- spec/javascript/drag_policy.spec.js
+++ spec/javascript/drag_policy.spec.js
@@ -5,26 +5,30 @@ import Rack from 'canvas/irv/view/Rack'
 import RackObject from 'canvas/irv/view/RackObject'
 import 'contrib/knockout.js'
 
-// I've taken this from some live front end data, there may be some unnecessary fields
-const deviceData = {
-    "id": 1,
-    "name": "comp102",
-    "buildStatus": "IN_PROGRESS",
-    "cost": "$0.00",
-    "instances": [],
-    "column": 0,
-    "row": 0,
-    "facing": "f",
-    "template": {
-        "images": {},
-        "width": 1,
-        "height": 1,
-        "rotateClockwise": true
-    },
-    "focused": false
-}
+describe("DragPolicy.filter : real models", () => {
+    // Test DragPolicy.filter using real models.  This is a pain to setup so
+    // only a single test exists using these models.
 
-const chassisData = {
+    // I've taken this from some live front end data, there may be some unnecessary fields
+    const deviceData = {
+        "id": 1,
+        "name": "comp102",
+        "buildStatus": "IN_PROGRESS",
+        "cost": "$0.00",
+        "instances": [],
+        "column": 0,
+        "row": 0,
+        "facing": "f",
+        "template": {
+            "images": {},
+            "width": 1,
+            "height": 1,
+            "rotateClockwise": true
+        },
+        "focused": false
+    }
+
+    const chassisData = {
         "template": {
             "id": 1,
             "name": "Large",
@@ -51,86 +55,131 @@ const chassisData = {
         "uStart":0,
         "uEnd":3,
         "instances":[]
-};
-
-const rackData = {
-    "owner": {
-        "id": 5,
-        "name": "bilbo",
-        "login": "bilbo"
-    },
-    "template": {
-        "id": 1,
-        "name": "rack",
-        "rackable": 3,
-        "images": {
-            "rack_rear_top": "misc_rack_600_rear_top.png",
-            "rack_repeat_1": "misc_rack_600_repeat_1.png",
-            "rack_front_top": "misc_rack_600_front_top.png",
-            "rack_rear_bottom": "misc_rack_600_rear_bottom.png",
-            "rack_front_bottom": "misc_rack_600_front_bottom.png"
-        },
-        "height": 42,
-        "depth": 2,
-        "padding_left": 0,
-        "padding_right": 0,
-        "padding_top": 0,
-        "padding_bottom": 0,
-        "simple": "true"
-    },
-    "id": 1,
-    "name": "Rack-1",
-    "uHeight": 42,
-    "buildStatus": "IN_PROGRESS",
-    "cost": "$0.00",
-    "nextRackId": 2,
-    "chassis": [chassisData]
-}
-
-// lots of class variables that require values before instantiating objects works
-beforeAll(() => {
-    // Pretend images, with minimal required data
-    const fakeImage = { height: 99 }
-    Rack.IMAGES_BY_TEMPLATE[1] = {};
-    Rack.IMAGES_BY_TEMPLATE[1].slices = {
-        front: {
-            top    : fakeImage,
-            btm    : fakeImage,
-            repeat : fakeImage,
-        },
-        rear: {
-            top    : fakeImage,
-            btm    : fakeImage,
-            repeat : fakeImage,
-        }
     };
 
-    AssetManager.CACHE[RackObject.IMAGE_PATH + chassisData.template.images.front] = fakeImage;
-    AssetManager.CACHE[RackObject.IMAGE_PATH + chassisData.template.images.rear] = fakeImage;
+    const rackData = {
+        "owner": {
+            "id": 5,
+            "name": "bilbo",
+            "login": "bilbo"
+        },
+        "template": {
+            "id": 1,
+            "name": "rack",
+            "rackable": 3,
+            "images": {
+                "rack_rear_top": "misc_rack_600_rear_top.png",
+                "rack_repeat_1": "misc_rack_600_repeat_1.png",
+                "rack_front_top": "misc_rack_600_front_top.png",
+                "rack_rear_bottom": "misc_rack_600_rear_bottom.png",
+                "rack_front_bottom": "misc_rack_600_front_bottom.png"
+            },
+            "height": 42,
+            "depth": 2,
+            "padding_left": 0,
+            "padding_right": 0,
+            "padding_top": 0,
+            "padding_bottom": 0,
+            "simple": "true"
+        },
+        "id": 1,
+        "name": "Rack-1",
+        "uHeight": 42,
+        "buildStatus": "IN_PROGRESS",
+        "cost": "$0.00",
+        "nextRackId": 2,
+        "chassis": [chassisData]
+    }
 
-    // Pretend front end element
-    RackObject.RACK_GFX = {"containerEl": {}};
+    // lots of class variables that require values before instantiating objects works
+    beforeAll(() => {
+        // Pretend images, with minimal required data
+        const fakeImage = { height: 99 }
+        Rack.IMAGES_BY_TEMPLATE[1] = {};
+        Rack.IMAGES_BY_TEMPLATE[1].slices = {
+            front: {
+                top    : fakeImage,
+                btm    : fakeImage,
+                repeat : fakeImage,
+            },
+            rear: {
+                top    : fakeImage,
+                btm    : fakeImage,
+                repeat : fakeImage,
+            }
+        };
 
-    RackObject.MODEL = new ViewModel();
-    RackObject.MODEL.deviceLookup({"racks": {}, "devices": {}, "chassis": {}})
+        AssetManager.CACHE[RackObject.IMAGE_PATH + chassisData.template.images.front] = fakeImage;
+        AssetManager.CACHE[RackObject.IMAGE_PATH + chassisData.template.images.rear] = fakeImage;
 
-    // Dont try to draw images, as no canvas to draw. We could mock/ manually set one, but not really
-    // relevant to this test.
-    const mockedFunction = jest.fn();
-    jest.spyOn(Rack.prototype, 'setImageCache').mockImplementation(mockedFunction);
+        // Pretend front end element
+        RackObject.RACK_GFX = {"containerEl": {}};
+
+        RackObject.MODEL = new ViewModel();
+        RackObject.MODEL.deviceLookup({"racks": {}, "devices": {}, "chassis": {}})
+
+        // Dont try to draw images, as no canvas to draw. We could mock/ manually set one, but not really
+        // relevant to this test.
+        const mockedFunction = jest.fn();
+        jest.spyOn(Rack.prototype, 'setImageCache').mockImplementation(mockedFunction);
+    });
+
+    afterAll(() => {
+        Rack.IMAGES_BY_TEMPLATE = {};
+        AssetManager.CACHE = {};
+        RackObject.RACK_GFX = null;
+        RackObject.MODEL = null;
+        jest.restoreAllMocks();
+    });
+
+    test("returns draggee's rack if included in list of racks", () => {
+        // There may be an easier or better way to generate a machine directly, but there are so, so many dependencies
+        const rack = new Rack(rackData, "racks", 1);
+        const machine = rack.children[0].children[0];
+        expect(DragPolicy.filter(machine, [rack])).toEqual([rack]);
+    });
 });
 
-afterAll(() => {
-    Rack.IMAGES_BY_TEMPLATE = {};
-    AssetManager.CACHE = {};
-    RackObject.RACK_GFX = null;
-    RackObject.MODEL = null;
-    jest.restoreAllMocks();
-});
+describe("DragPolicy.filter : mocks", () => {
+    // Test DragPolicy.filter using fake models.  Setting up the real models is
+    // a pain, so we use fake models here to ease writing additional test
+    // cases.
 
-test("filter function returns array containing just device's rack", () => {
-   // There may be an easier or better way to generate a machine directly, but there are so, so many dependencies
-   const rack = new Rack(rackData, "racks", 1);
-   const machine = rack.children[0].children[0];
-   expect(DragPolicy.filter(machine)).toEqual([rack]);
+    class Rack {
+        constructor(id) {
+            this.id = id;
+        }
+    }
+
+    class Draggee {
+        constructor(id, rack) {
+            this.id = id;
+            this._rack = rack;
+        }
+
+        rack() {
+            return this._rack;
+        }
+    }
+
+    const rack1 = new Rack("r1");
+    const rack2 = new Rack("r2");
+
+    test("returns empty list if draggee has no rack", () => {
+        const draggee = new Draggee("d1");
+        expect(DragPolicy.filter(draggee, [rack1, rack2])).toEqual([]);
+    });
+
+    test("returns draggee's rack if included in list of racks", () => {
+        const draggee = new Draggee("d1", rack2);
+        expect(DragPolicy.filter(draggee, [rack1, rack2])).toEqual([rack2]);
+    });
+
+    test.skip("returns empty list if draggee's rack is not included in list of racks", () => {
+        // This test current fails, the return is `[rack2]`, but that isn't
+        // included in the list of available racks.  I think that is probably a bug
+        // in the implementation.
+        const draggee = new Draggee("d1", rack2);
+        expect(DragPolicy.filter(draggee, [rack1])).toEqual([]);
+    });
 });
