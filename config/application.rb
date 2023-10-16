2e17cd36958c0f9076cc31e1a6fd5636c8e0f710
Ben Armston <ben.armston@alces-flight.com>
2023-10-16 19:38:13 +0100

    Some pending specs can pass now

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module CtApp
     config.jwt_aud = 'alces-ct'
 
     # The base URL to use for the concertim metric reporting daemon.
-    config.metric_daemon_url = ENV.fetch("METRIC_DAEMON_URL", "http://localhost:3000")
+    config.metric_daemon_url = ENV.fetch("METRIC_DAEMON_URL", "http://localhost:3000/")
 
     # Display a fake invoice if ENV['FAKE_INVOICE'] is set.  Otherwise the
     # concertim-openstack-service will be contacted to provide the invoice.
