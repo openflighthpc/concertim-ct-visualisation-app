f063c3c5b75b5951d94d41c09d2663dd72491750
timalces <tim.brackfield@alces-software.com>
2023-11-03 12:15:41 +0000

    better initial transmit logic

--- app/channels/interactive_rack_view_channel.rb
+++ app/channels/interactive_rack_view_channel.rb
@@ -3,6 +3,7 @@ class InteractiveRackViewChannel < ApplicationCable::Channel
 
   def subscribed
     stream_for current_user
+    ensure_confirmation_sent # without this, after_subscribe may run before confirmation is sent
   end
 
   private
@@ -11,6 +12,6 @@ class InteractiveRackViewChannel < ApplicationCable::Channel
     all_racks = Irv::HwRackServices::Index.call(current_user)
     # all_racks = {Racks: {Rack: []}}
     all_racks[:action] = "latest_full_data"
-    InteractiveRackViewChannel.broadcast_to(current_user, all_racks)
+    transmit(all_racks)
   end
 end
