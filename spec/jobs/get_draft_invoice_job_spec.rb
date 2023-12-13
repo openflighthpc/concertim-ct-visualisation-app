ae2708c48f543a82c885e282134a95afeffbf282
Ben Armston <ben.armston@alces-flight.com>
2023-12-13 19:00:18 +0000

    fix specs

--- spec/jobs/get_draft_invoice_job_spec.rb
+++ spec/jobs/get_draft_invoice_job_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe GetDraftInvoiceJob, type: :job do
 
     it "contains invoice config" do
       expect(subject[:invoice]).to eq({
-        "billing_acct_id" => user.billing_acct_id,
+        "billing_account_id" => user.billing_acct_id,
         "target_date" => "#{Date.today.year}-#{Date.today.month}-#{"%02d" % Date.today.day}",
       })
     end
