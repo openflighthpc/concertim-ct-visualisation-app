41ab2441c447a300c9754ac19ecd87da1a2edb26
Ben Armston <ben.armston@alces-software.com>
2023-12-20 15:57:08 +0000

    Enable browser validations
    Browser validations for `required`, `min` and `max` have been enabled.
    
    Validations for `pattern`, `minlength` and `maxlength` can be opted into
    on a per-input basis.
    
    Enabling `pattern` automatically doesn't really work.  The regex
    needs to be anchored to the start and end of the string.  Rails
    complains if `^`and `$` are used for this, instead wanting `\A` and
    `\Z`.  JavaScript doesn't recognize those assertions.  That difference
    of regular expression interpretation makes its automatic inclusion
    awkward.  We'd also, need to specify the pattern in the help or hint
    text too.
    
    I don't like the behaviour of the `maxlength` browser validation.  It
    silently prevents additional characters from being entered.  This is
    easy to miss especially when pasting.  A better UX is to allow the input
    but then complain on form submission.  The roundtrip validation provides
    us with that behaviour.
    
    The `minlength` validation is left as opt-in for parity with
    `maxlength`.  Perhaps, it ought to be enabled automatically.
    
    Missing validations from the user model have been added to make it
    consistent with the database schema.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -46,7 +46,7 @@ SimpleForm.setup do |config|
     b.optional :pattern
 
     # Calculates min and max from length validations for numeric inputs
-    b.optional :min_max
+    b.use :min_max
 
     # Calculates readonly automatically from readonly attributes
     b.optional :readonly
@@ -128,7 +128,7 @@ SimpleForm.setup do |config|
   # in this configuration, which is recommended due to some quirks from different browsers.
   # To stop SimpleForm from generating the novalidate option, enabling the HTML5 validations,
   # change this configuration to true.
-  config.browser_validations = false
+  config.browser_validations = true
 
   # Custom mappings for input types. This should be a hash containing a regexp
   # to match as key, and the input type that will be used when the field name
