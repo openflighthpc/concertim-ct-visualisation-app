60f87c55035f95ac4e9886311e3c855a450704f5
timalces <tim.brackfield@alces-software.com>
2023-12-21 13:09:02 +0000

    added credit deposit model validation tests

---
+++ spec/models/credit_deposit_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe CreditDeposit, type: :model do
+  subject { build(:credit_deposit) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without an amount" do
+    subject.amount = nil
+    expect(subject).to have_error(:amount, :blank)
+  end
+
+  it "is not valid if amount negative" do
+    subject.amount = -1
+    expect(subject).to have_error(:amount, :greater_than)
+  end
+
+  it "is not valid if amount zero" do
+    subject.amount = 0
+    expect(subject).to have_error(:amount, :greater_than)
+  end
+end
