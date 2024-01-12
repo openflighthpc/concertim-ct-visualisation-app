0d502931020aeb16b2602874d0336ec20f981050
timalces <tim.brackfield@alces-software.com>
2024-01-12 15:58:25 +0000

    added edit team page

--- app/presenters/team_presenter.rb
+++ app/presenters/team_presenter.rb
@@ -14,4 +14,22 @@ class TeamPresenter < Presenter
   def formatted_credits
     '%.2f' % o.credits
   end
+
+  def project_id_form_hint
+    form_hint(:project_id)
+  end
+
+  def billing_acct_id_form_hint
+    form_hint(:billing_acct_id)
+  end
+
+  private
+
+  def form_hint(attribute)
+    if o.send(attribute).blank?
+      I18n.t("simple_form.customisations.hints.team.edit.#{attribute}.blank")
+    else
+      I18n.t("simple_form.customisations.hints.team.edit.#{attribute}.present")
+    end
+  end
 end
