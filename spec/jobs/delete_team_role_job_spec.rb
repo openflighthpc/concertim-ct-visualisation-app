7145484e0402f0b25528b0693a7f3e349adbe34c
timalces <tim.brackfield@alces-software.com>
2024-01-25 15:07:36 +0000

    added updated and delete team job specs

--- spec/jobs/delete_team_role_job_spec.rb
+++ spec/jobs/delete_team_role_job_spec.rb
@@ -1,22 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe UpdateTeamRoleJob, type: :job do
+RSpec.describe DeleteTeamRoleJob, type: :job do
   let(:stubs) { Faraday::Adapter::Test::Stubs.new }
   let!(:team_role) { create(:team_role, role: "member") }
-  let(:new_role) { "admin" }
   let(:cloud_service_config) { create(:cloud_service_config) }
-  let(:update_team_roles_path) { "/update_team_role" }
-  let(:expected_url) {
-    "#{cloud_service_config.user_handler_base_url}#{update_team_roles_path}"
-  }
+  let(:delete_team_role_path) { "/delete_team_role" }
+  let(:path) { "#{cloud_service_config.user_handler_base_url}/delete_team_role" }
 
-  subject { UpdateTeamRoleJob::Runner.new(cloud_service_config: cloud_service_config, team_role: team_role, new_role: new_role) }
+  subject { DeleteTeamRoleJob::Runner.new(cloud_service_config: cloud_service_config, team_role: team_role) }
 
   describe "url" do
-    subject { super().send(:url) }
+    before(:each) do
+      class << subject
+        public :connection
+        public :path
+      end
+    end
 
-    it "uses the correct ip, port and path" do
-      expect(subject).to eq expected_url
+    it "uses the ip and port given in the config" do
+      expect(subject.connection.url_prefix.to_s).to eq "#{cloud_service_config.user_handler_base_url}/"
+    end
+
+    it "uses a hard-coded path" do
+      expect(subject.path).to eq "/delete_team_role"
+    end
+  end
+
+  describe "path" do
+    subject { super().send(:path) }
+
+    it "uses the path" do
+      expect(subject).to eq delete_team_role_path
     end
   end
 
@@ -32,10 +46,11 @@ RSpec.describe UpdateTeamRoleJob, type: :job do
                                         })
     end
 
-    it "contains the team role's project and billing ids" do
+    it "contains the team role's details" do
       expect(subject[:team_role]).to be_a Hash
       expect(subject[:team_role][:user_id]).to eq team_role.user.cloud_user_id
       expect(subject[:team_role][:project_id]).to eq team_role.team.project_id
+      expect(subject[:team_role][:role]).to eq team_role.role
     end
   end
 
@@ -44,34 +59,34 @@ RSpec.describe UpdateTeamRoleJob, type: :job do
 
     shared_examples "makes a request to the middleware" do
       it "makes a request to the middleware" do
-        runner = described_class::Runner.new(team_role: team_role, new_role: new_role, cloud_service_config: cloud_service_config)
+        runner = described_class::Runner.new(team_role: team_role, cloud_service_config: cloud_service_config)
         expect(described_class::Runner).to receive(:new)
-                                             .with(hash_including(team_role: team_role, new_role: new_role, cloud_service_config: cloud_service_config))
+                                             .with(hash_including(team_role: team_role, cloud_service_config: cloud_service_config))
                                              .and_return(runner)
         allow(runner).to receive(:call).and_call_original
-        described_class.perform_now(team_role, new_role, cloud_service_config)
+        described_class.perform_now(team_role, cloud_service_config)
         expect(runner).to have_received(:call)
       end
     end
 
     context "when the request is successful" do
       before(:each) do
-        stubs.post(expected_url) { |env| [ 204, {}, "No Content"] }
+        stubs.delete(path) { |env| [ 204, {}, "No Content"] }
         allow_any_instance_of(described_class::Runner).to receive(:test_stubs).and_return(stubs)
       end
 
       include_examples "makes a request to the middleware"
 
-      it "updates role" do
+      it "deletes role" do
         expect {
-          described_class.perform_now(team_role, new_role, cloud_service_config, test_stubs: stubs)
-        }.to change(team_role, :role)
+          described_class.perform_now(team_role, cloud_service_config, test_stubs: stubs)
+        }.to change(TeamRole, :count).by(-1)
       end
     end
 
     context "when the request is unsuccessful" do
       before(:each) do
-        stubs.post(expected_url) { |env| [ 500, {}, {"error" => "Some error message"}] }
+        stubs.delete(path) { |env| [ 500, {}, {"error" => "Some error message"}] }
         allow_any_instance_of(described_class::Runner).to receive(:test_stubs).and_return(stubs)
       end
 
@@ -79,8 +94,8 @@ RSpec.describe UpdateTeamRoleJob, type: :job do
 
       it "does not change the role" do
         expect {
-          described_class.perform_now(team_role, new_role ,cloud_service_config)
-        }.not_to change(team_role, :role)
+          described_class.perform_now(team_role,cloud_service_config)
+        }.not_to change(TeamRole, :count)
       end
     end
   end
