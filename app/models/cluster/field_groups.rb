21ea897aacd4fcd7d5b0e9768468166c395bf221
Ben Armston <ben.armston@alces-flight.com>
2024-02-05 10:49:01 +0000

    Better construction of ungrouped parameters

--- app/models/cluster/field_groups.rb
+++ app/models/cluster/field_groups.rb
@@ -1,5 +1,5 @@
 class Cluster::FieldGroups
-  attr_reader :fields, :groups, :ungrouped_fields
+  attr_reader :fields, :groups
 
   def initialize(cluster, groups, fields)
     @groups = groups.map do |group_definition|
@@ -9,16 +9,25 @@ class Cluster::FieldGroups
       Cluster::Field.new(id, details)
     end
 
-    @ungrouped_fields = []
+    ungrouped_fields = []
     @fields.each do |field|
       group = @groups.detect { |g| g.contains_field?(field.id) }
       if group.nil?
-        @ungrouped_fields << field
+        ungrouped_fields << field
       else
         group.add(field)
       end
     end
-    @ungrouped_fields.sort_by!(&:order)
+
+    unless ungrouped_fields.empty?
+      ungrouped_group = Cluster::FieldGroup.new(
+        label: 'Cluster Parameters',
+        description: '',
+        parameters: ungrouped_fields.sort_by(&:order).map(&:id),
+      )
+      ungrouped_fields.each { |g| ungrouped_group.add(g) }
+      @groups.unshift(ungrouped_group)
+    end
   end
 
   def each(&block)
