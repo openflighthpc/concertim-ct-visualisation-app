4ac2a605adec830763cbd19c0687b2949abaf095
timalces <tim.brackfield@alces-software.com>
2024-02-20 12:12:54 +0000

    updated migration order post rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_19_161225) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_100609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
