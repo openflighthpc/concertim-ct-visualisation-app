130fcdd237d8ad2ecb52ce9393a786c04cf0f6ec
James Muscat <james.muscat@alces-software.com>
2024-02-23 12:11:51 +0000

    Validate `VolumeDetails` is used with the volume template.

--- app/models/device/volume_details.rb
+++ app/models/device/volume_details.rb
@@ -1,2 +1,15 @@
 class Device::VolumeDetails < Device::Details
+
+  validate :device_uses_volume_template
+
+  private
+
+  def device_uses_volume_template
+    reload_device
+    return unless device.present?
+    unless device.template.tag == 'volume'
+      self.errors.add(:device, 'must use the `volume` template if it has a Device::VolumeDetails')
+    end
+  end
+
 end
