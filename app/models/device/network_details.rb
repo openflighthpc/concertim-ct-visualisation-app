31a78128e335bf034a5ad1293fcc815982fd973d
James Muscat <james.muscat@alces-software.com>
2024-02-23 11:50:48 +0000

    Validate that `NetworkDetails` is only used with a network template.

--- app/models/device/network_details.rb
+++ app/models/device/network_details.rb
@@ -1,2 +1,15 @@
 class Device::NetworkDetails < Device::Details
+
+  validate :device_uses_network_template
+
+  private
+
+  def device_uses_network_template
+    reload_device
+    return unless device.present?
+    unless device.template.tag == 'network'
+      self.errors.add(:device, 'must use the `network` template if it has a Device::NetworkDetails')
+    end
+  end
+
 end
---
+++ spec/models/device/network_details_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Device::NetworkDetails, type: :model do
+  let(:user) { create(:user) }
+  let!(:rack_template) { create(:template, :rack_template) }
+  let!(:rack) { create(:rack, user: user, template: rack_template) }
+  let(:location) { create(:location, rack: rack) }
+  let(:chassis) { create(:chassis, location: location, template: template) }
+
+  let(:device_template) { create(:template, :device_template) }
+  let(:network_template) { create(:template, :network_device_template) }
+
+  let(:device) { create(:device, chassis: chassis, details: described_class.new) }
+
+  subject { device.details }
+
+  describe 'validations' do
+    context 'for a device using the network template' do
+      let(:template) { network_template }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'for a device using the compute device template' do
+      let(:template) { device_template }
+
+      it { is_expected.not_to be_valid }
+      it { is_expected.to have_error :device, 'must use the `network` template if it has a Device::NetworkDetails' }
+
+      it 'also shows error on device' do
+        expect(device).not_to be_valid
+        expect(device).to have_error :details, 'is invalid'
+      end
+    end
+
+  end
+end
