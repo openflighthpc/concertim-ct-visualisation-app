52fc32bf0488166bc38a0cc102dbbd916fab5f01
timalces <tim.brackfield@alces-software.com>
2024-02-27 13:26:10 +0000

    refactored cluster form error cell to allow for errors without inputs

--- app/cells/cluster_form_errors_cell.rb
+++ app/cells/cluster_form_errors_cell.rb
@@ -6,25 +6,44 @@ class ClusterFormErrorsCell < Cell::ViewModel
 
   private
 
-  def has_errors?
-    error_count > 0
+  def attributes_without_input_field
+    [:team]
   end
 
-  def error_count
+  def has_input_field_errors?
+    input_field_error_count > 0
+  end
+
+  def input_field_error_count
     if @cluster.errors.any?
       # If the cluster has any errors set against it, it is expected that these
       # will contain any field errors too.
-      @cluster.errors.count
+      @cluster.errors.count - inputless_error_count
 
     elsif @cluster.fields.any? { |f| !f.errors.empty? }
       # If the cluster does not have any errors, it is still possible that the
       # fields do.  These can be set from the cluster builder response.
       @cluster.fields
-        .select { |f| !f.errors.empty? }
-        .map { |f| f.errors.count }
-        .sum
+              .select { |f| !f.errors.empty? }
+              .map { |f| f.errors.count }
+              .sum
     else
       0
     end
+  end
+
+  def has_errors_without_input_field?
+    @cluster.errors.any? && attributes_without_input_field.any? { |attribute| !@cluster.errors[attribute].empty? }
+  end
+
+  def inputless_error_count
+    attributes_without_input_field.select { |attribute| !@cluster.errors[attribute].empty? }.length
+  end
+
+  def inputless_errors_text
+    @cluster.errors.select { |error| attributes_without_input_field.include?(error.attribute) }
+            .map(&:full_message)
+            .join("; ")
+
   end
 end
