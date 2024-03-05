2a12fef37cbf6ab049d78b88812c0b648bd9de9d
James Muscat <james.muscat@alces-software.com>
2024-03-05 14:29:26 +0000

    Revert "Modify API to maintain support for current middleware."
    This reverts commit 6b8d3e95ef198312a3e0dc1e049e1bed463a32e2.

--- app/controllers/api/v1/nodes_controller.rb
+++ app/controllers/api/v1/nodes_controller.rb
@@ -28,9 +28,7 @@ class Api::V1::NodesController < Api::V1::ApplicationController
   end
 
   def device_params
-    permitted_params.except(:location, :template_id, :details, 
-      :public_ips, :private_ips, :ssh_key, :login_user, :volume_details
-    )
+    permitted_params.except(:location, :template_id, :details)
   end
 
   def location_params
@@ -40,26 +38,12 @@ class Api::V1::NodesController < Api::V1::ApplicationController
   end
 
   def details_params
-    permitted_params.fetch(:details, {}).tap do |details|
-      if details.empty?
-        legacy_params = permitted_params.slice(
-          :public_ips,
-          :private_ips,
-          :ssh_key,
-          :login_user,
-          :volume_details
-        )
-        legacy_params[:type] = 'Device::ComputeDetails'
-        return legacy_params
-      end
-    end
+    permitted_params.fetch(:details, {})
   end
 
   PERMITTED_PARAMS = [
-    "name", "description", "status", "cost", 
-    "public_ips", "private_ips", "ssh_key", "login_user",
-    "location" => %w[rack_id start_u facing],
-  ] << {metadata: {}, details: {}, volume_details: {}}
+    "name", "description", "status", "cost", "location" => %w[rack_id start_u facing]
+  ] << {metadata: {}, details: {}}
 
   def permitted_params
     params.require(:device).permit(*PERMITTED_PARAMS)
--- app/views/api/v1/devices/show.rabl
+++ app/views/api/v1/devices/show.rabl
@@ -6,11 +6,11 @@ child(:template, if: @include_full_template_details) do
   extends 'api/v1/templates/show'
 end
 
-glue :details do |details|
+child details: :details do |details|
   extends "api/v1/devices/details/#{details.class.name.split('::').last.underscore}"
-  #node :type do |details|
-  #  details.class.name
-  #end
+  node :type do |details|
+    details.class.name
+  end
 end
 
 attribute :template_id, unless: @include_full_template_details
