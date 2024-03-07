bd82c52465eedf16dacb7d56d4ba945ad766d0a3
timalces <tim.brackfield@alces-software.com>
2024-03-07 17:54:27 +0000

    don't initial create job

--- app/jobs/create_single_user_team_job.rb
+++ app/jobs/create_single_user_team_job.rb
@@ -1,6 +1,5 @@
 class CreateSingleUserTeamJob < ApplicationJob
   queue_as :default
-  retry_on ::ActiveModel::ValidationError, wait: :polynomially_longer, attempts: 10
 
   def perform(user, cloud_service_config)
     team = nil
@@ -9,13 +8,13 @@ class CreateSingleUserTeamJob < ApplicationJob
     ActiveRecord::Base.transaction do
       team = Team.new(name: "#{user.login}_team", single_user: true)
       unless team.save
-        logger.info("Unable to create team for #{user.login} #{team.errors.full_messages.join("; ")}")
+        logger.info("Unable to create team for #{user.login} #{team.errors.details}")
         raise ActiveModel::ValidationError, team
       end
 
       team_role = TeamRole.new(team: team, user: user, role: "admin")
       unless team_role.save
-        logger.info("Unable to create team role for #{user.login} #{team_role.errors.full_messages.join("; ")}")
+        logger.info("Unable to create team role for #{user.login} #{team_role.errors.details}")
         logger.info("Rolling back creation of team #{team.name}")
         raise ActiveModel::ValidationError, team_role
       end
